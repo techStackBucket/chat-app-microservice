f8f01a9de190fe422f87ff64d226f048ebf3bbc4
Hamdi Bayhan <hamdibayhan@gmail.com>
2021-02-21 01:21:34 +0300

    Added body-parser configuration in index.js file

--- authentication/src/index.js
+++ authentication/src/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.listen(port, _ => {
   console.log(`Authentication service listening at http://localhost:${port}`)
