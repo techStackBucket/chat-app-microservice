4385fba8bdc6ddf6287b1ce81f9ca378dcb0b155
Hamdi Bayhan <hamdibayhan@gmail.com>
2021-02-22 02:33:31 +0300

    db.js file was created with mongoose configuration

---
+++ authentication/src/helpers/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const options = { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+}
+
+mongoose.set('useCreateIndex', true);
+mongoose.connect(
+    process.env.MONGODB_URI, 
+    options
+);
+
+module.exports = {
+    User: require('../models/user')
+};
