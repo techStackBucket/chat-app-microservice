47bfdfde6e4777c929c3871e8570ea30b0c7ab84
Hamdi Bayhan <hamdibayhan@gmail.com>
2021-02-23 01:52:26 +0300

    Code refactored

--- authentication/src/routes.js
+++ authentication/src/routes.js
@@ -4,19 +4,22 @@ const { resultCodes } = require('./enums');
 
 const router = express.Router();
 
-router.post('/register', async function (req, res, next) {
+router.post('/register', register)
+router.post('/login', login)
+
+module.exports = router;
+
+function register(req, res, next) {
     const { email, password } = req.body
-    createUser({email, password})
+    createUser({ email, password })
         .then(_ => res.status(201).json({ result: resultCodes.SUCCESS }))
         .catch(err => next(err))
-})
+}
 
-router.post('/login', async function (req, res, next) {
-    await authenticate(req.body)
-      .then(data => data ? res.json({ result: resultCodes.SUCCESS, data}) : res.status(400).json({ result: resultCodes.ERROR, message: 'Email or password is incorrect' }))
-      .catch(err => next(err))
-  })
-
-module.exports = {
-    router
+function login(req, res, next) {
+    authenticate(req.body)
+        .then(data => data
+            ? res.json({ result: resultCodes.SUCCESS, data })
+            : res.status(400).json({ result: resultCodes.ERROR, message: 'Email or password is incorrect' }))
+        .catch(err => next(err))
 }
