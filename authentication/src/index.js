8d68351dd5cb4ca739c52b266aca3e7980330076
Hamdi Bayhan <hamdibayhan@gmail.com>
2021-02-23 23:29:11 +0300

    Added custom global error handler

--- authentication/src/index.js
+++ authentication/src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const router = require('./routes')
+const errorHandler = require('./helpers/errorHandler')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -10,6 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use('/api/v1', router);
+app.use(errorHandler)
 
 app.listen(port, _ => {
   console.log(`Authentication service listening at http://localhost:${port}`)
