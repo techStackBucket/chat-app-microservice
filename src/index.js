ddc43d3234dc5bb9127af4dafb401a1084b34fbe
Faruk Aygun <faygun89@gmail.com>
2021-02-27 13:22:16 +0000

    list, get and delete features have been added.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const {postUser, patchUser} = require('./controllers');
+const {postUser, patchUser, deleteUser, getUser, listUser} = require('./controllers');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const makeCallback =  require('./express-callback');
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 const apiRoot = process.env.API_ROOT
 console.log(apiRoot);
 
 app.post(`${apiRoot}/users`, makeCallback(postUser));
 app.patch(`${apiRoot}/users/:id`, makeCallback(patchUser));
+app.delete(`${apiRoot}/users/:id`, makeCallback(deleteUser));
+app.get(`${apiRoot}/users/:id`, makeCallback(getUser));
+app.get(`${apiRoot}/users`, makeCallback(listUser));
 
 app.listen(3000, ()=>{console.log("app is running")});
 
